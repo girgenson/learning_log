dcfda078bde62887c8aa1b6a36ef6928a0623785
glu <vadimgirgenson@yandex.ru>
2020-11-21 22:27:40 +0300

    Set DEBUG based on environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5i)5gvdk&3v=7mi#^sggfv_1b+a3!pfqn3jrp_njax+sd06dr!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -135,7 +138,4 @@ LOGIN_URL = '/users/login/'
 import django_heroku
 django_heroku.settings(locals())
 
-if os.environ.get('DEBUG') == 'TRUE':
-    DEBUG = True
-elif os.environ.get('DEBUG') == 'FALSE':
-    DEBUG = False
+
